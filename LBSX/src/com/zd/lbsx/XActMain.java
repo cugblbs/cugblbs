f74d3538b3eb909c03de1453627fb6f5068e12c6
afalb <ab1002782737@126.com>
2014-09-21 19:51:32 +0800

    sz 2.0

--- LBSX/src/com/zd/lbsx/XActMain.java
+++ LBSX/src/com/zd/lbsx/XActMain.java
@@ -3,20 +3,23 @@ package com.zd.lbsx;
 /*
  * create by Juice Zhu 2014.7.31
  */
+import android.content.Intent;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.RadioGroup;
+
 import com.zd.lbsx.fragments.XFgBase;
 import com.zd.lbsx.fragments.XFgFind;
 import com.zd.lbsx.fragments.XFgInfo;
 import com.zd.lbsx.fragments.XFgMy;
 import com.zd.lbsx.fragments.XFgRoute;
 
-import android.content.Intent;
-import android.view.View;
-import android.widget.RadioGroup;
-
 public class XActMain extends XActBase implements
 		android.widget.RadioGroup.OnCheckedChangeListener {
 
 	private RadioGroup radioGroup;
+	private XFgBase xfg=null;
 
 	@Override
 	public void onClick(View v) {
@@ -51,7 +54,7 @@ public class XActMain extends XActBase implements
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.content_frame, fg).commit();
 	}
-	
+
 	@Override
 	public void onCheckedChanged(RadioGroup group, int checkedId) {
 		XFgBase fg = null;
@@ -61,6 +64,7 @@ public class XActMain extends XActBase implements
 			break;
 		case R.id.rb_info:
 			fg = new XFgInfo();
+			//registInterface((XFgInfo) fg);
 			break;
 		case R.id.rb_my:
 			fg = new XFgMy();
@@ -76,4 +80,40 @@ public class XActMain extends XActBase implements
 				.replace(R.id.content_frame, fg).commit();
 	}
 
+//	public interface BackWeview {
+//		boolean backwebview();
+//	}
+//
+//	private void registInterface(XFgInfo fg) {
+//		xfg = fg;
+//	}
+//
+//	@Override
+//	public void onBackPressed() {
+//		if (xfg != null) {
+//			boolean islastone = ((XFgInfo) xfg).backwebview();
+//			if (!islastone) {
+//				this.finish();
+//			}
+//		}
+//	}
+
+	 @Override
+	 public boolean onKeyDown(int keyCode, KeyEvent event) {
+	 //监听按下返回键
+	 if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() ==
+	 KeyEvent.ACTION_DOWN) {
+	 /*已经是最后一个fragment
+	 * getSupportFragmentManager()或者getFragmentManager()
+	 * 具体要看你add to back stack 是用哪个*/
+	 //if no more history in stack
+	 Log.i("s", "main_on_back");
+	 if (XActMain.this.getFragmentManager().getBackStackEntryCount() == 0) {
+	 //显示退出框业务逻辑
+	 finish();
+	 return true;
+	 }
+	 }
+	 return super.onKeyDown(keyCode, event);
+	 }
 }
